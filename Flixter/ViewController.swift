41d57a2fda05de4b962e89281f3eddf7005202e3
Daize Njounkeng <njounkengdaizem@berea.edu>
2023-03-06 13:39:04 -0500

    Initial commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Daize Njounkeng on 3/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
