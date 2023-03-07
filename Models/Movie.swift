1dd04cac805f3b9f98126a0515c382b32d3c45e1
Daize Njounkeng <njounkengdaizem@berea.edu>
2023-03-07 17:38:16 -0500

    basic layout complete

---
+++ Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Flixter
+//
+//  Created by Daize Njounkeng on 3/6/23.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let artistName: String
+    let artworkUrl100: URL
+}
